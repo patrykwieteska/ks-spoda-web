dcf0c4c11df64d53911f4adea3f70990373d3cfe
patrykwieteska <patrykwieteskapw@gmail.com>
2023-10-19 21:46:07 +0200

    heroku config v5

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// app.use(express.static(__dirname + '/dist/<app-name>'));
-// app.get('/*', function(req,res) {
-// res.sendFile(path.join(__dirname+
-// '/dist/<app-name>/index.html'));});
-// app.listen(process.env.PORT || 8080);
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.listen(process.env.PORT || 4200);
+app.use(express.static(__dirname + '/dist/ks-spoda-web'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/ks-spoda-web/index.html'));});
+app.listen(process.env.PORT || 8080);
